c31082300b594d69cd5e25a4d3a51e0b62be99de
ngttt <1754050092tien@ou.edu.vn>
2020-02-15 21:00:10 +0700

    session1

---
+++ js/components.js
@@ -0,0 +1,6 @@
+const components = {};
+components.register = `
+<div>Register Screen</div> 
+`;
+
+components.login = `<div> <h1> Login Screen <h1> <div>`;
---
+++ js/view.js
@@ -0,0 +1,12 @@
+const view = {
+};
+view.setActiveScreen = (screenName) => {
+ switch (screenName) {
+     case 'register':
+         document.getElementById('app').innerHTML = components.register;
+        break;
+    case 'login':
+        document.getElementById('app').innerHTML = components.login;
+        break;
+ }
+}
