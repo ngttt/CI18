005b935f7047880fe940f0c16d427fbbaa2f2fa2
ngttt <1754050092tien@ou.edu.vn>
2020-02-25 22:39:57 +0700

    tt

---
+++ session1/class/js/initFirebase.js
@@ -0,0 +1,15 @@
+
+  //coppy trên trang firebase https://firebase.google.com/docs/web/setup
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhpo26OkEVBifjIoIYh7i-tQ-11INedoA",
+    authDomain: "ci-18-4b7f1.firebaseapp.com",
+    databaseURL: "https://ci-18-4b7f1.firebaseio.com",
+    projectId: "ci-18-4b7f1",
+    storageBucket: "ci-18-4b7f1.appspot.com",
+    messagingSenderId: "481441207901",
+    appId: "1:481441207901:web:04b7aaa89e4f6496779fb4",
+    measurementId: "G-GLP42WCGSQ"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); // khởi tạp file page
